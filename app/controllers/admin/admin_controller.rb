2ebc1b04c2a66b88dbac1b2f97dd054d9d80fdf3
konyan <devkonyan@gmail.com>
2020-06-17 04:59:00 +0630

    add admin controller

---
+++ app/controllers/admin/admin_controller.rb
@@ -0,0 +1,11 @@
+class Admin::AdminController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+
   namespace :admin do
+    get '', to: 'admin#index', as: ''
     resources :charities
   end
 
