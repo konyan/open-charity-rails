bc1e31890494af337dba96f4357e27a7f2f8e175
konyan <devkonyan@gmail.com>
2020-06-19 03:21:19 +0630

    fix dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,12 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @month_total = Transaction.where('extract(month from created_at) = ?', 6).sum("net_amount")
+    @month_total_count = Transaction.where('extract(month from created_at) = ?', 6).count
+    @all_total = Transaction.all.sum("net_amount")
+    @all_total_count = Transaction.all.count
+    @today_total = Transaction.where("DATE(created_at) = DATE(?)", Time.now).sum("net_amount")
+    @today_total_count =Transaction.where("DATE(created_at) = DATE(?)", Time.now).count
 
   end
 
